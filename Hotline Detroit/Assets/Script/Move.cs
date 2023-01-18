b3aaaa494b7b7c3a2eb629a56a19579cab21e8fb
Honungsgrabben <axel.eklof@elev.ga.lbs.se>
2023-01-18 12:45:24 +0100

    Move/Cam

---
+++ Hotline Detroit/Assets/Script/Move.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float sneakSpeed = 4.0f;
+
+    public float walkSpeed = 7.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        Screen.SetResolution(1920, 1080, false);
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            walkSpeed = sneakSpeed;
+        }
+        else
+            walkSpeed = walkSpeed;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            walkSpeed = 7;
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * walkSpeed, vertical * walkSpeed);
+    }
+}
