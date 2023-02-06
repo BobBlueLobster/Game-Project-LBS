320c47bb91440786aaf10fd7e4b68f8b226af4f7
Konrad Janik <konrad.janik1@learnet.se>
2023-02-06 11:42:49 +0100

    basic grid class
    added a basic grid class + CodeMonkeyUtils (important!!!)

--- Hotline Detroit/Assets/Script/Scipts for testing/Testing.cs
+++ Hotline Detroit/Assets/Script/Scipts for testing/Testing.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
 
     private void Start()
     {
-        GridCM grid = new GridCM(4, 2);
+        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
     }
 }
