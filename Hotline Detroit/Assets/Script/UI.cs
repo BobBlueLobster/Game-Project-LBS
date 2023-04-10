5a3264350c8826d8be1fedc96609628ba5370fda
Honungsgrabben <axel.eklof@elev.ga.lbs.se>
2023-04-10 21:57:50 +0200

    RevolverUI

--- Hotline Detroit/Assets/Script/UI.cs
+++ Hotline Detroit/Assets/Script/UI.cs
@@ -6,40 +6,67 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public TextMeshProUGUI ammo;
-    public TextMeshProUGUI mag;
-
     public Animator animator;
-
-    public TextMeshProUGUI dialogue1;
 
     public Player playerScript;
 
     public Gun gunScript;
 
-    public Image crack;
+    public Image _image;
 
-    public Sprite crack1;
-    public Sprite crack2;
-    public Sprite crack3;
-    public Sprite crack4;
-    public Sprite crack5;
+    public GameObject revolverUI;
+
+    public Sprite a_zero;
+    public Sprite a_one;
+    public Sprite a_two;
+    public Sprite a_three;
+    public Sprite a_four;
+    public Sprite a_five;
+    public Sprite a_six;
+
 
     void Start()
     {
-        crack = GameObject.Find("Crack").GetComponent<Image>();
-
         playerScript = GameObject.Find("Sprite").GetComponent<Player>();
         gunScript = GameObject.Find("Gun").GetComponent<Gun>();
 
-        animator = GetComponent<Animator>();
+        _image = revolverUI.GetComponent<Image>();
 
-        ammo = GameObject.Find("AmmoCount").GetComponent<TextMeshProUGUI>();
-        mag = GameObject.Find("Mag").GetComponent<TextMeshProUGUI>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        if(gunScript.magazineCur == 0)
+        {
+            _image.sprite = a_zero;
+        }
+        if (gunScript.magazineCur == 1)
+        {
+            _image.sprite = a_one;
+        }
+        if (gunScript.magazineCur == 2)
+        {
+            _image.sprite = a_two;
+        }
+        if (gunScript.magazineCur == 3)
+        {
+            _image.sprite = a_three;
+        }
+        if (gunScript.magazineCur == 4)
+        {
+            _image.sprite = a_four;
+        }
+        if (gunScript.magazineCur == 5)
+        {
+            _image.sprite = a_five;
+        }
+        if (gunScript.magazineCur == 6)
+        {
+            _image.sprite = a_six;
+        }
+
+        /*
         if(playerScript.ammoCount < 10)
         {
             ammo.SetText("0"+playerScript.ammoCount.ToString());
@@ -48,7 +75,9 @@ public class UI : MonoBehaviour
             ammo.SetText(playerScript.ammoCount.ToString());
 
         mag.SetText(gunScript.magazineCur + "/");
+        */
 
+        /*
         if(playerScript.curHP < 10 && playerScript.curHP >= 8)
         {
             crack.gameObject.SetActive(true);
@@ -79,7 +108,9 @@ public class UI : MonoBehaviour
             crack.gameObject.SetActive(false);
 
         }
+        */
 
+        /*
         if(playerScript.killScore == 1)
         {
             dialogue1.SetText("[I didn't sign up for all this, I just want to revenge my loved one...]");
@@ -95,5 +126,6 @@ public class UI : MonoBehaviour
             dialogue1.SetText("[I get this ache...and I thought it was for sex, but it's to tear everything to f*kcing pieces...]");
             animator.SetBool("Dialogue3", true);
         }
+        */
     }
 }
